93cff3dae269c3222d1ff483cd6c6c5e03575485
Davide Cavallini <davidecavallini1987@gmail.com>
2021-09-15 17:32:34 +0200

    ,

--- Resources/views/home/show.blade.php
+++ Resources/views/home/show.blade.php
@@ -5,10 +5,13 @@
 
     <div class="container-large">
         <div class="col-md-12 p-0 text-center">
-            <img style="width:100vw;max-height:100vh;opacity:0.6" class="img-fluid" src="{{ Theme::asset('pub_theme::img/homebackground.jpg') }}" alt="portale dei sindacati" />
+            <img style="width:100vw;height:100vh;opacity:0.6" class="img-fluid"
+                src="{{ Theme::asset('pub_theme::img/homebackground.jpg') }}" alt="portale dei sindacati" />
             <div class="centered" style="width:100vw;background: rgba(0,0,0,0.5);
-            padding-left: 2vw;
-            padding-right: 2vw;"><span style="color:white;font-size:10vh;font-weight:bold;text-shadow:2px 2px rgb(39, 39, 39);">BENVENUTO</span></div>
+                            padding-left: 2vw;
+                            padding-right: 2vw;"><span
+                    style="color:white;font-size:7vh;font-weight:bold;text-shadow:2px 2px rgb(39, 39, 39);">BENVENUTO</span>
+            </div>
         </div>
     </div>
 
