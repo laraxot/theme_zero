ce69992fa379ff92a8146605fb116dfbcd5c25ff
Davide Cavallini <davidecavallini1987@gmail.com>
2021-09-15 11:38:19 +0200

    .

---
+++ Resources/views/home/show.blade.php
@@ -0,0 +1,16 @@
+@extends('clubreport::layouts.app')
+@section('content')
+
+
+
+    <div class="container-large">
+        <div class="col-md-12 p-0 text-center">
+            <img style="width:100vw;max-height:100vh;opacity:0.6" class="img-fluid" src="{{ Theme::asset('pub_theme::img/homebackground.jpg') }}" alt="portale dei sindacati" />
+            <div class="centered" style="width:100vw;background: rgba(0,0,0,0.5);
+            padding-left: 2vw;
+            padding-right: 2vw;"><span style="color:white;font-size:10vh;font-weight:bold;text-shadow:2px 2px rgb(39, 39, 39);">BENVENUTO</span></div>
+        </div>
+    </div>
+
+
+@endsection
---
+++ Resources/views/home/store/acts/scheda_caricata.blade.php
@@ -0,0 +1,1 @@
+@include('clubreport::admin.store.acts.scheda_caricata')
