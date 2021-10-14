9fcc69ae374a56fee8df790a76b5eb14cc100909
Davide Cavallini <davidecavallini1987@gmail.com>
2021-10-14 17:05:30 +0200

    up

--- Resources/views/profile/index.blade.php
+++ Resources/views/profile/index.blade.php
@@ -19,52 +19,67 @@ $rows = \Modules\ClubReport\Models\Profile::all();
 @endphp
 
 @section('content')
-    @php
-    //dddx(get_defined_vars());
-    if ($rows_err != null) {
-        exit('<h3><pre>' . $rows_err->getMessage() . '</pre></h3>');
-        return;
-    }
-    @endphp
 
-    @foreach ($rows as $key => $row)
+    <div class="row">
+        <div class="col-md-12 mt-3 mb-4 text-center">
+            @php
+                //dddx(1 === \Auth::user()->perm->perm_type);
+            @endphp
 
-        @if ($loop->first)
-            <table class="table table-hover table-striped">
-                <thead>
-                    <tr>
-                        @foreach ($fields as $field)
-                            <td>{{ str_replace('_', ' ', $field->name) }}</td>
-                        @endforeach
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
-        @endif
-        <tr>
-            @foreach ($fields as $field)
-                <td>
-                    {!! Theme::inputFreeze(['row' => $row, 'field' => $field]) !!}
-                    @if ($loop->first)
-                        @foreach ($_panel->itemActions() as $act)
-                            {{-- togliere una graffa per parte --}}
-                            {{-- !!$act->btn(['row'=>$row])!! --}}
-                        @endforeach
-                    @endif
-                </td>
+
+            <h1>Gestione Utenti</h1>
+
+            @php
+                //dddx(get_defined_vars());
+                if ($rows_err != null) {
+                    exit('<h3><pre>' . $rows_err->getMessage() . '</pre></h3>');
+                    return;
+                }
+            @endphp
+
+            @foreach ($rows as $key => $row)
+
+                @if ($loop->first)
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                @foreach ($fields as $field)
+                                    @if (!empty($field->label))
+                                        <td>{{ str_replace('_', ' ', $field->label) }}</td>
+                                    @else
+                                        <td>{{ str_replace('_', ' ', $field->name) }}</td>
+                                    @endif
+                                @endforeach
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                @endif
+                <tr>
+                    @foreach ($fields as $field)
+                        <td>
+                            {!! Theme::inputFreeze(['row' => $row, 'field' => $field]) !!}
+                            @if ($loop->first)
+                                @foreach ($_panel->itemActions() as $act)
+                                    {{-- togliere una graffa per parte --}}
+                                    {{-- !!$act->btn(['row'=>$row])!! --}}
+                                @endforeach
+                            @endif
+                        </td>
+                    @endforeach
+                    <td>
+                        {{-- togliere una graffa per parte --}}
+                        @php
+                            //dddx($row);
+                        @endphp
+                        {!! Form::bsBtnCrud(['row' => $row]) !!}
+                    </td>
+                </tr>
+                @if ($loop->last)
+                    </tbody>
+                    </table>
+                @endif
             @endforeach
-            <td>
-                {{-- togliere una graffa per parte --}}
-                @php
-                    //dddx($row);
-                @endphp
-                {!! Form::bsBtnCrud(['row' => $row]) !!}
-            </td>
-        </tr>
-        @if ($loop->last)
-            </tbody>
-            </table>
-        @endif
-    @endforeach
-
+        </div>
+    </div>
 @endsection
